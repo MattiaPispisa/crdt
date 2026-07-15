4939b991f04d7ca72589fca1a35ef8c04fbbdbb4
Mattia Pispisa <mattia.pispisa@gmail.com>
2026-07-15 22:50:09 +0200

    generated

--- packages/crdt_socket_sync/client_example/lib/generated.dart
+++ packages/crdt_socket_sync/client_example/lib/generated.dart
@@ -2,4 +2,4 @@
 // ignore_for_file: constant_identifier_names, non_constant_identifier_names
 
 String crdt_socket_sync_version = '0.5.0';
-String crdt_lf_version = '3.2.1';
+String crdt_lf_version = '3.4.0';
